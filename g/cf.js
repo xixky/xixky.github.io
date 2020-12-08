e80a55048504c9ec5a77c4d81b03aae71697e66a
xixka <xka@live.com>
2020-12-08 16:30:30 +0800

    以后开发

--- g/cf.js
+++ g/cf.js
@@ -3,7 +3,7 @@
 /**
  * static files (404.html, sw.js, conf.js)
  */
-const ASSET_URL = 'https://hunshcn.github.io/gh-proxy/'
+const ASSET_URL = 'https://xixky.github.io/g/'
 // 前缀，如果自定义路由为example.com/gh/*，将PREFIX改为 '/gh/'，注意，少一个杠都会错！
 const PREFIX = '/'
 // git使用cnpmjs镜像、分支文件使用jsDelivr镜像的开关，0为关闭，默认开启
@@ -72,19 +72,27 @@ async function fetchHandler(e) {
     if (path.search(exp1) === 0 || !Config.cnpmjs && (path.search(exp3) === 0 || path.search(exp4) === 0)) {
         return httpHandler(req, path)
     } else if (path.search(exp2) === 0) {
-        if (Config.jsdelivr){
+        //文件会跳转至JsDelivr
+        if (Config.jsdelivr) {
+            // 使用jsDelivr镜像
             const newUrl = path.replace('/blob/', '@').replace(/^(?:https?:\/\/)?github\.com/, 'https://cdn.jsdelivr.net/gh')
             return Response.redirect(newUrl, 302)
-        }else{
+        } else {
+            //走cf直连
             path = path.replace('/blob/', '/raw/')
             return httpHandler(req, path)
         }
     } else if (path.search(exp3) === 0) {
+        // 使用git clone
         const newUrl = path.replace(/^(?:https?:\/\/)?github\.com/, 'https://github.com.cnpmjs.org')
         return Response.redirect(newUrl, 302)
     } else if (path.search(exp4) === 0) {
+        //https://raw.githubusercontent.com/rclone/rclone/master/CONTRIBUTING.md
+        // https://cdn.jsdelivr.net/gh/rclone/rclone@master/CONTRIBUTING.md
         const newUrl = path.replace(/(?<=com\/.+?\/.+?)\/(.+?\/)/, '@$1').replace(/^(?:https?:\/\/)?raw\.githubusercontent\.com/, 'https://cdn.jsdelivr.net/gh')
         return Response.redirect(newUrl, 302)
+    }else if (path.search("\/") === 0) {
+        return httpHandler(req, path)
     } else {
         return fetch(ASSET_URL + path)
     }
